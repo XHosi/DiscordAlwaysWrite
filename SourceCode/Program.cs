db7caa78f19f8cff8e7177f1c1cb0c18f1c2602a
XHosi <107482591+XHosi@users.noreply.github.com>
2023-12-25 16:31:47 +0100

    Update Program.cs
    downgradev from .net 8 to .net 4.8

--- SourceCode/Program.cs
+++ SourceCode/Program.cs
@@ -1,22 +1,31 @@
-﻿namespace DAPI
+using System.Net.Http;
+using System;
+using System.Threading.Tasks;
+namespace DAPI
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Welcome to my small discord api app\nthis technically breaks discord TOS\nso don't blame me if you get banned\n\n");
             string authorizationToken = ValidateInput("Enter your discord token:", 70);
             string ChannelID = ValidateInput("Enter ChannelID:", 19);
-            var client = new HttpClient { BaseAddress = new Uri($"https://discord.com/api/v9/channels/{ChannelID}/typing") };
-            while (true)
-            {
-                var request = new HttpRequestMessage(HttpMethod.Post, "");
-                request.Headers.Add("Authorization", authorizationToken);
-                var response = await client.SendAsync(request);
+            var client = new HttpClient();
+            client.BaseAddress = new Uri($"https://discord.com/api/v9/channels/{ChannelID}/typing");
 
-                Console.WriteLine(response.IsSuccessStatusCode ? "Typing..." : $"Failed to send typing status. Status code: {response.StatusCode}");
-                await Task.Delay(6000);
-            }
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var request = new HttpRequestMessage(HttpMethod.Post, "");
+                    request.Headers.Add("Authorization", authorizationToken);
+                    var response = await client.SendAsync(request);
+
+                    Console.WriteLine(response.IsSuccessStatusCode ? "Typing..." : $"Failed to send typing status. Status code: {response.StatusCode}");
+                    await Task.Delay(6000);
+                }
+            });
+            Console.ReadLine(); // Keep the console window open
         }
         static string ValidateInput(string prompt, int expectedLength)
         {
